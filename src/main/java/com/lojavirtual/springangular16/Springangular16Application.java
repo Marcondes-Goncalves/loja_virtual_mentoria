914c345082334e541b910a4322eeccbd37bd59b1
Marcondes <marcondesgoncalves159@gmail.com>
2024-12-28 19:51:02 -0300

    projeto criado

---
+++ src/main/java/com/lojavirtual/springangular16/Springangular16Application.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.springangular16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springangular16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springangular16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojavirtual/springangular16/Springangular16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.springangular16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springangular16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
